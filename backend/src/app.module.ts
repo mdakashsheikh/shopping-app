40211341d896e5ba71e255f30a585ef4a6373417
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-12-28 23:01:36 +0600

    adding backend and frontend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production'
+
+        return {
+          pinoHttp: {
+            transport: isProduction 
+              ? undefined 
+              : {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true,
+                },
+              lebel: isProduction ? 'info' : 'debug'
+            }
+          }
+        }
+      }
+    }),
+    ConfigModule.forRoot(), UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
